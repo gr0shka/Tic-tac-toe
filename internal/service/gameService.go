c11bf04ee4944b0b3f882b132bace9b1eeb1a1ab
grush <girois19@gmail.com>
2026-09-03 23:13:54 +0500

    declare interface GameService

--- internal/service/gameService.go
+++ internal/service/gameService.go
@@ -3,5 +3,7 @@ package service
 import "Tic-Tac-Toy/internal/models"
 
 type GameService interface {
-	GetNextTurn(gb models.GameBoard, realPlayer) int
+	GetNextTurn(gb models.GameBoard) models.GameBoard
+	ValidateBoard(oldBoard, newBoard models.GameBoard) error
+	IsEnded(gb models.GameBoard) (int, bool)
 }
