5954a9559578a0e429a8632ec219f98fc4f16847
grush <girois19@gmail.com>
2026-09-03 23:25:38 +0500

    change declare func ValidateBoard

--- internal/service/gameService.go
+++ internal/service/gameService.go
@@ -4,6 +4,6 @@ import "Tic-Tac-Toy/internal/models"
 
 type GameService interface {
 	GetNextTurn(gb models.GameBoard) models.GameBoard
-	ValidateBoard(oldBoard, newBoard models.GameBoard) error
+	ValidateBoard(oldB, newB models.GameBoard) error
 	IsEnded(gb models.GameBoard) (int, bool)
 }
