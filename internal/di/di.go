2aaa46947c4f9389a1e9885547b006b651acb16e
grush <girois19@gmail.com>
2026-09-09 23:58:01 +0500

    add info for di what struct impl interface

--- internal/di/di.go
+++ internal/di/di.go
@@ -5,6 +5,7 @@ import (
 
 	appService "github.com/gr0shka/Tic-tac-toe/internal/application/service"
 	gameService "github.com/gr0shka/Tic-tac-toe/internal/domain/service"
+	"github.com/gr0shka/Tic-tac-toe/internal/repository"
 	"github.com/gr0shka/Tic-tac-toe/internal/repository/mapstore"
 	transportHttp "github.com/gr0shka/Tic-tac-toe/internal/transport/http/handler"
 	"go.uber.org/fx"
@@ -13,9 +14,18 @@ import (
 func CreateApp() fx.Option {
 	return fx.Options(
 		fx.Provide(
-			mapstore.NewMapRepository,
-			gameService.NewGameService,
-			appService.NewAppService,
+			fx.Annotate(
+				mapstore.NewMapRepository,
+				fx.As(new(repository.Repository)),
+			),
+			fx.Annotate(
+				gameService.NewGameService,
+				fx.As(new(gameService.GameService)),
+			),
+			fx.Annotate(
+				appService.NewAppService,
+				fx.As(new(appService.AppService)),
+			),
 			transportHttp.NewHandler,
 		),
 		fx.Invoke(
