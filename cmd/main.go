7fda9f8f88f89237710170774e860bdaab66927a
grush <girois19@gmail.com>
2026-09-09 23:17:26 +0500

    add di

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	appService "github.com/gr0shka/Tic-tac-toe/internal/application/service"
-	domainService "github.com/gr0shka/Tic-tac-toe/internal/domain/service"
-	"github.com/gr0shka/Tic-tac-toe/internal/repository/mapStore"
-	transportHttp "github.com/gr0shka/Tic-tac-toe/internal/transport/http"
+	"github.com/gr0shka/Tic-tac-toe/internal/di"
+	"go.uber.org/fx"
 )
 
 func main() {
-	repo := mapStore.NewMapRepository()
-	gs := domainService.NewGameService()
-	as := appService.NewAppService(gs, repo)
-	handler := transportHttp.NewHandler(as)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/game/{uuid}", handler.NextTurn)
-	mux.HandleFunc("/create", handler.NewGame)
-	mux.HandleFunc("/get/{uuid}", handler.GetGame)
-	http.ListenAndServe(":8080", mux)
+	fx.New(di.CreateApp()).Run()
 }
--- internal/di/di.go
+++ internal/di/di.go
@@ -1,1 +1,36 @@
 package di
+
+import (
+	"net/http"
+
+	appService "github.com/gr0shka/Tic-tac-toe/internal/application/service"
+	gameService "github.com/gr0shka/Tic-tac-toe/internal/domain/service"
+	"github.com/gr0shka/Tic-tac-toe/internal/repository/mapStore"
+	transportHttp "github.com/gr0shka/Tic-tac-toe/internal/transport/http"
+	"go.uber.org/fx"
+)
+
+func CreateApp() fx.Option {
+	return fx.Options(
+		fx.Provide(
+			mapStore.NewMapRepository,
+			gameService.NewGameService,
+			appService.NewAppService,
+			transportHttp.NewHandler,
+		),
+		fx.Invoke(
+			CreateMuxAndStartServer,
+		),
+	)
+}
+
+func CreateMuxAndStartServer(h *transportHttp.Handler) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /game/{uuid}", h.NextTurn)
+	mux.HandleFunc("GET /create", h.NewGame)
+	mux.HandleFunc("GET /get/{uuid}", h.GetGame)
+
+	go func() {
+		http.ListenAndServe(":8080", mux)
+	}()
+}
