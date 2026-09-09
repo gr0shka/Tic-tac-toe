71290bcec4c44314ea71d13c88f7e673e3f184cd
grush <girois19@gmail.com>
2026-09-09 12:15:40 +0500

    add repository interface

---
+++ internal/repository/repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"github.com/google/uuid"
+	"github.com/gr0shka/Tic-tac-toe/internal/domain/models"
+)
+
+type Repository interface {
+	Save(cg models.CurrentGame) error
+	Get(id uuid.UUID) (models.CurrentGame, error)
+}
