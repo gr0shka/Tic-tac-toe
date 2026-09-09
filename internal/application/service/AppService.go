886706d45cf5f3ae90c0c66fe0852cd93685e56a
grush <girois19@gmail.com>
2026-09-09 12:15:51 +0500

    add repository interface

---
+++ internal/application/service/AppService.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/google/uuid"
+	"github.com/gr0shka/Tic-tac-toe/internal/domain/models"
+)
+
+type AppService interface {
+	CreateGame() (*models.CurrentGame, error)
+	ProcessPlayerMove(
+		id uuid.UUID,
+		board [models.BoardSize][models.BoardSize]int,
+	) (*models.CurrentGame, error)
+	GetGame(id uuid.UUID) (*models.CurrentGame, error)
+}
