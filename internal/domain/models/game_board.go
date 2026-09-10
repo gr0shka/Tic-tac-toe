fa1d1960936668dfab05b5845cb11b4b83ec146c
grush <girois19@gmail.com>
2026-09-10 16:34:12 +0500

    delete mutex from struct

--- internal/domain/models/game_board.go
+++ internal/domain/models/game_board.go
@@ -1,7 +1,5 @@
 package models
 
-import "sync"
-
 const (
 	EmptyCell = -1
 
@@ -15,7 +13,6 @@ type GameBoard struct {
 	board        [BoardSize][BoardSize]int
 	numberOfTurn int
 	players      [CountPlayers]Player
-	mu           sync.RWMutex
 }
 
 func NewGameBoard() *GameBoard {
@@ -30,70 +27,44 @@ func NewGameBoard() *GameBoard {
 	return &GameBoard{
 		board:        board,
 		numberOfTurn: 0,
-		mu:           sync.RWMutex{},
 	}
 }
 
 func (gb *GameBoard) Clone() *GameBoard {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	clone := &GameBoard{
 		board:        gb.board,
 		numberOfTurn: gb.numberOfTurn,
 		players:      gb.players,
-		mu:           sync.RWMutex{},
 	}
 	return clone
 }
 
 func (gb *GameBoard) AddPlayers(player1, player2 Player) {
-	gb.mu.Lock()
-	defer gb.mu.Unlock()
-
 	gb.players[FirstPlayer] = player1
 	gb.players[SecondPlayer] = player2
 }
 
 func (gb *GameBoard) SetTurnNumber(turnNumber int) {
-	gb.mu.Lock()
-	defer gb.mu.Unlock()
-
 	gb.numberOfTurn = turnNumber
 }
 
 func (gb *GameBoard) Set(x, y int, v int) {
-	gb.mu.Lock()
-	defer gb.mu.Unlock()
-
 	gb.board[x][y] = v
 }
 
 func (gb *GameBoard) Get(x, y int) int {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	return gb.board[x][y]
 }
 
 func (gb *GameBoard) GetBoard() [BoardSize][BoardSize]int {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	return gb.board
 }
 
 func (gb *GameBoard) GetPlayers() [CountPlayers]Player {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	return gb.players
 }
 
 func (gb *GameBoard) NextPlayer() (Player, bool) {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	turnNumber := gb.numberOfTurn % CountPlayers
 
 	if turnNumber >= len(gb.players) {
@@ -104,22 +75,13 @@ func (gb *GameBoard) NextPlayer() (Player, bool) {
 }
 
 func (gb *GameBoard) NextTurn() {
-	gb.mu.Lock()
-	defer gb.mu.Unlock()
-
 	gb.numberOfTurn++
 }
 
 func (gb *GameBoard) GetNumberOfTurns() int {
-	gb.mu.RLock()
-	defer gb.mu.RUnlock()
-
 	return gb.numberOfTurn
 }
 
 func (gb *GameBoard) SetBoard(board [BoardSize][BoardSize]int) {
-	gb.mu.Lock()
-	defer gb.mu.Unlock()
-
 	gb.board = board
 }
