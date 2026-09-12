ef4d52aa7c71ad910b48de6514c11e39a450b212
grush <girois19@gmail.com>
2026-09-12 22:37:30 +0500

    add errors

--- internal/domain/models/errors.go
+++ internal/domain/models/errors.go
@@ -1,1 +1,12 @@
 package models
+
+import "errors"
+
+var (
+	ErrInvalidTurn          = errors.New("invalid turn")
+	ErrWrongNumberOfTurns   = errors.New("wrong number of turns")
+	ErrAlteredPreviousMoves = errors.New("altered previous moves")
+	ErrMultipleMoves        = errors.New("multiple moves")
+	ErrNoMoveMade           = errors.New("no move made")
+	ErrWrongPlayerMove      = errors.New("wrong player move")
+)
