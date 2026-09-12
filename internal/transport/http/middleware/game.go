931b78569e5b622d4e4d700b25bbaf1417fa88cd
grush <girois19@gmail.com>
2026-09-12 22:57:27 +0500

    change add header on set

--- internal/transport/http/middleware/game.go
+++ internal/transport/http/middleware/game.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func SetHeaders(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		h.ServeHTTP(w, r)
 	})
 }
