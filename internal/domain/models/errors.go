20487ec822fc127bda7b5c41ccbd8bda5bf3e2d1
grush <girois19@gmail.com>
2026-09-12 22:41:59 +0500

    add errors

--- internal/domain/models/errors.go
+++ internal/domain/models/errors.go
@@ -3,10 +3,12 @@ package models
 import "errors"
 
 var (
-	ErrInvalidTurn          = errors.New("invalid turn")
-	ErrWrongNumberOfTurns   = errors.New("wrong number of turns")
-	ErrAlteredPreviousMoves = errors.New("altered previous moves")
-	ErrMultipleMoves        = errors.New("multiple moves")
-	ErrNoMoveMade           = errors.New("no move made")
-	ErrWrongPlayerMove      = errors.New("wrong player move")
+	ErrInvalidTurn             = errors.New("invalid turn")
+	ErrWrongNumberOfTurns      = errors.New("wrong number of turns")
+	ErrAlteredPreviousMoves    = errors.New("altered previous moves")
+	ErrMultipleMoves           = errors.New("multiple moves")
+	ErrNoMoveMade              = errors.New("no move made")
+	ErrWrongPlayerMove         = errors.New("wrong player move")
+	ErrNotFound                = errors.New("not found")
+	ErrFailedCalculateNextTurn = errors.New("failed to calculate next turn")
 )
