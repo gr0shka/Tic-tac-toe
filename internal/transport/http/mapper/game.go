54cf4e78ca60953ca1fefc4006d247b73d13f0fb
grush <girois19@gmail.com>
2026-09-14 10:24:17 +0500

    add imports

--- internal/transport/http/mapper/game.go
+++ internal/transport/http/mapper/game.go
@@ -1,5 +1,10 @@
 package mapper
 
+import (
+	"github.com/gr0shka/Tic-tac-toe/internal/domain/game"
+	"github.com/gr0shka/Tic-tac-toe/internal/transport/http/dto"
+)
+
 func ToGameBoardResponse(cg *game.CurrentGame, winner int, ended bool) dto.GameBoardResponse {
 	return dto.GameBoardResponse{
 		ID:          cg.ID(),
