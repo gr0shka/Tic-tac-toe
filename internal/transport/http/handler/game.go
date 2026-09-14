10717bf265a50ed38e4edb31b0695f4eff0f2c10
grush <girois19@gmail.com>
2026-09-14 14:00:21 +0500

    GetGame: change response on dto

--- internal/transport/http/handler/game.go
+++ internal/transport/http/handler/game.go
@@ -92,7 +92,9 @@ func (h *Handler) GetGame(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := mapper.ToGameBoardResponse(game, -1, false)
+
 	w.WriteHeader(http.StatusOK)
 	encoder := json.NewEncoder(w)
-	encoder.Encode(game.Board())
+	encoder.Encode(data)
 }
