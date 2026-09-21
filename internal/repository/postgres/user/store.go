aca6f0d80307f784fe137d068ce8cf1537ebbd13
grush <girois19@gmail.com>
2026-09-22 00:06:47 +0500

    create interface UserRepository and impl

---
+++ internal/repository/postgres/user/store.go
@@ -0,0 +1,65 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/gr0shka/Tic-tac-toe/internal/domain/user"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type repository struct {
+	db *pgxpool.Pool
+}
+
+func New(db *pgxpool.Pool) *repository {
+	return &repository{
+		db: db,
+	}
+}
+
+func (r repository) Save(ctx context.Context, user *user.User) error {
+	query := "INSERT INTO users(id, login, password) VALUES($1, $2, $3)"
+
+	u := DomainToDTO(user)
+
+	_, err := r.db.Exec(ctx, query, u.ID, u.Login, u.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r repository) Get(ctx context.Context, id uuid.UUID) (*user.User, error) {
+	query := "SELECT * FROM users WHERE id = $1"
+
+	rows, err := r.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[DTO])
+	if err != nil {
+		return nil, err
+	}
+
+	return DTOtoDomain(u), nil
+}
+
+func (r repository) GetByLogin(ctx context.Context, login string) (*user.User, error) {
+	query := "SELECT * FROM users WHERE login = $1"
+
+	rows, err := r.db.Query(ctx, query, login)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[DTO])
+	if err != nil {
+		return nil, err
+	}
+
+	return DTOtoDomain(u), nil
+}
---
+++ internal/usecase/user/repository.go
@@ -0,0 +1,14 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/gr0shka/Tic-tac-toe/internal/domain/user"
+)
+
+type UserRepository interface {
+	Save(ctx context.Context, user *user.User) error
+	Get(ctx context.Context, id uuid.UUID) (*user.User, error)
+	GetByLogin(ctx context.Context, login string) (*user.User, error)
+}
