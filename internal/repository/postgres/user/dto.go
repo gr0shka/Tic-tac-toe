10de190ffed486507201ebdd202d91c32988af62
grush <girois19@gmail.com>
2026-09-25 20:34:55 +0500

    rename dto to userDTO

--- internal/repository/postgres/user/dto.go
+++ internal/repository/postgres/user/dto.go
@@ -2,7 +2,7 @@ package user
 
 import "github.com/google/uuid"
 
-type DTO struct {
+type userDTO struct {
 	ID       uuid.UUID `db:"id"`
 	Login    string    `db:"login"`
 	Password string    `db:"password"`
