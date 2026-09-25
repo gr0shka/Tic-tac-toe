7352936ee87058b015f5d271b835743a647d0f65
grush <girois19@gmail.com>
2026-09-25 20:35:02 +0500

    rename dto to userDTO

--- internal/repository/postgres/user/mapper.go
+++ internal/repository/postgres/user/mapper.go
@@ -2,10 +2,10 @@ package user
 
 import "github.com/gr0shka/Tic-tac-toe/internal/domain/user"
 
-func DTOtoDomain(u DTO) *user.User {
+func DTOtoDomain(u userDTO) *user.User {
 	return user.New(u.ID, u.Login, u.Password)
 }
 
-func DomainToDTO(u *user.User) DTO {
-	return DTO{u.ID(), u.Login(), u.Password()}
+func DomainToDTO(u *user.User) userDTO {
+	return userDTO{u.ID(), u.Login(), u.Password()}
 }
