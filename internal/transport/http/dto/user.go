46e114385967e5fa989ff6bef7bfcd3e1cb739db
grush <girois19@gmail.com>
2026-09-25 20:36:23 +0500

    add UserInfoRespone

--- internal/transport/http/dto/user.go
+++ internal/transport/http/dto/user.go
@@ -1,6 +1,13 @@
 package dto
 
+import "github.com/google/uuid"
+
 type SignUpRequest struct {
 	Login    string `json:"login"`
 	Password string `json:"password"`
+}
+
+type UserInfoResponse struct {
+	ID    uuid.UUID `json:"id"`
+	Login string    `json:"login"`
 }
